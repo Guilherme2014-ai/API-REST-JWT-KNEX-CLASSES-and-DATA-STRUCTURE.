ca077de8f1e50ce98f17ec8069be6b5c109695b9
Guilherme <77082955+Guilherme2014-ai@users.noreply.github.com>
2021-07-04 09:58:58 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ var knex = require('knex')({
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : 'Celso_bixa2014',
+      password : '*******',
       database : 'user_api'
     }
 });
